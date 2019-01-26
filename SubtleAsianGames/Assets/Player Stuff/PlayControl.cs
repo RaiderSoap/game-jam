84f4a2be68acbfd1551e6da4ee4b486e73ae1dfa
Alfred Lam <aylam@ucsc.edu>
2019-01-26 12:37:14 -0800

    playcontrolscript

---
+++ SubtleAsianGames/Assets/Player Stuff/PlayControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayControl : MonoBehaviour
+{
+    void Update()
+  {
+    if(Input.GetKeyDown(KeyCode.UpArrow)) {
+      Debug.Log("Up");
+    }      
+    else if(Input.GetKeyDown(KeyCode.DownArrow)) {
+      Debug.Log("Down");
+    }
+    else if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+      Debug.Log("Left");
+    }
+    else if(Input.GetKeyDown(KeyCode.RightArrow)) {
+      Debug.Log("Right");
+    }
+  }
+}
