f50c690f476f4b73cdd346cb2f9316e274167492
RaiderSoap <info@chivalrystudio.com>
2019-01-26 12:59:48 -0800

    Camera following the character

---
+++ SubtleAsianGames/Assets/Scripts/DynamicCameraController.cs
@@ -0,0 +1,26 @@
+﻿namespace Assets.Scripts
+{
+    using UnityEngine;
+
+    public class DynamicCameraController : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform playerA;
+
+        private Vector3 offset;
+
+        #region Unity Callbacks
+
+        private void Start()
+        {
+            this.offset = this.transform.position - this.playerA.position;
+        }
+
+        private void LateUpdate()
+        {
+            this.transform.position = this.playerA.transform.position + this.offset;
+        }
+
+        #endregion
+    }
+}
