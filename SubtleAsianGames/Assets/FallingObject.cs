c69fa8011ecb50fb81cbe1faafa519d2b7ab42df
Jeremy <jemtan@ucsc.edu>
2019-01-26 17:57:56 -0800

    Auto stash before merge of "development" and "origin/development"

--- SubtleAsianGames/Assets/FallingObject.cs
+++ SubtleAsianGames/Assets/FallingObject.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FallingObject : MonoBehaviour
 {
   public float fallSpeed = 5f;
   bool falling = true;
+
+  
   // Start is called before the first frame update
   void Start()
   {
@@ -20,6 +23,8 @@ public class FallingObject : MonoBehaviour
     if(col.gameObject.tag == "Ground") {
       Debug.Log("hit ground");
       falling = false;
+      ScoreScript.scoreValue = ScoreScript.scoreValue - 1;
+      Debug.Log(ScoreScript.scoreValue);
     }
   }
 
---
+++ SubtleAsianGames/Assets/ScoreScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ScoreScript : MonoBehaviour
+{    
+    
+    public static int scoreValue =  1;
+    Text score;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GetComponent<Text> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    
+        score.text = "Lives " + scoreValue;
+        kidsWin();
+    }
+    
+    void kidsWin()
+    {
+        if(scoreValue == 0){
+            SceneManager.LoadScene("KidsWIn Scene", LoadSceneMode.Single);
+        }
+    }
+}
